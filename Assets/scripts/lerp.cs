e06be49c91ec320ec251841febfc872f19a35dcb
CF8339816 <chris_french_3dga@hotmail.com>
2025-10-19 18:18:11 -0300

    debug
    could not  get the lerp to loop , rewrote code several times trying different wording . trying math.PingPong now

--- Assets/scripts/lerp.cs
+++ Assets/scripts/lerp.cs
@@ -1,73 +1,111 @@
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using static UnityEngine.UIElements.UxmlAttributeDescription;
 
 public class lerp : MonoBehaviour
 {
     [SerializeField] Vector3 startPosition;
-    [SerializeField] Vector3 targetPosition;
     [SerializeField] Vector3 endPosition;
-    [SerializeField] Vector3 currentTarget;
+    [SerializeField] float moveDuration = 1.0f; 
+    
 
-    [SerializeField] float moveDuration = 1.0f; // Time in seconds
-    [SerializeField]  float elapsedTime;
-     static bool movingToEnd = true;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         startPosition = transform.position;
-        currentTarget = endPosition;
-
-        //startPosition = transform.position;
-        //targetPosition = new Vector3(10, 3, 0);
-        //endPosition = new Vector3(0, 3, 0);
-
+        endPosition = new Vector3(10, 3, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        elapsedTime += Time.deltaTime;
+        float t = Mathf.PingPong(Time.time, moveDuration) / moveDuration; // gets movement value using PingPong math 
 
-        float t = elapsedTime / moveDuration;
-
-       
-
-
-        if (movingToEnd)
-        {
-            //target switch for return trip check
-            if (currentTarget == targetPosition)
-            {
-                transform.position = Vector3.Lerp(startPosition, endPosition, t);
-            }
-            else
-            {
-                transform.position = Vector3.Lerp(endPosition, startPosition, t);
-            }
-
-            //if (currentTarget == targetPosition)
-            //{
-            //    currentTarget = startPosition;
-            //}
-            //else
-            //{
-            //    currentTarget = targetPosition;
-            //}
-
-
-
-        }
-     
-        transform.position = Vector3.Lerp(startPosition, endPosition, t);
-
-
-
-
+        transform.position = Vector3.Lerp(startPosition, endPosition, t); // lerp using pingpong value for t
     }
 
 
 
 
+
+
+
+
+
+
+
+
+
+    /*   thisis theworking lerp  codde  unlooped
+     *   
+    //    [SerializeField] Vector3 startPosition;
+    //    [SerializeField] Vector3 endPosition;
+    //    [SerializeField] Vector3 currentTarget;
+    //    [SerializeField] float moveDuration = 1.0f; // Time in seconds
+    //    [SerializeField] float elapsedTime;
+    //    bool movingToEnd = true;
+
+    //    void Start()
+    //    {
+    //        startPosition = transform.position;
+    //        endPosition = new Vector3(10, 3, 0);
+    //        currentTarget = endPosition;
+    //    }
+
+    //    void Update()
+    //    {
+
+    //        elapsedTime += Time.deltaTime;
+    //        float t = elapsedTime / moveDuration;
+    //        transform.position = Vector3.Lerp(startPosition, endPosition, t);
+
+    //    }
+    *
+    */
+
 }
+
+
+
+//tried using this code based on a tutorial once i got the lerp working but it kept changing thetranslation to +-y3 rather than 10 X trying a different method  above
+
+//using UnityEngine;
+
+//public class LerpLooping : MonoBehaviour
+//{
+//    [SerializeField] Vector3 startPosition;
+//    [SerializeField] Vector3 endPosition;
+//    [SerializeField] Vector3 currentTarget;
+//    [SerializeField] float elapsedTime;
+//    [SerializeField] float moveDuration = 1.0f; // Time to destination
+//    [SerializeField] bool movingToEnd = true;
+
+
+
+//    void Start()
+//    {
+//        startPosition = new Vector3(10, 0, 0);
+//        endPosition = new Vector3(10, 3, 0);
+//        currentTarget = endPosition;
+//    }
+
+//    void Update()
+//    {
+//        elapsedTime += Time.deltaTime;
+//        float t = elapsedTime / moveDuration;
+
+//        if (movingToEnd) // Apply Lerp towards current destination
+//        {
+//            transform.position = Vector3.Lerp(startPosition, endPosition, t);
+//        }
+//        else
+//        {
+//            transform.position = Vector3.Lerp(endPosition, startPosition, t);
+//        }
+//        if (t >= 1.0f) // if path complete reset timer and reverse direction
+//        {
+
+//            elapsedTime = 0;
+//            movingToEnd = !movingToEnd;
+//        }
+//    }
+//}
